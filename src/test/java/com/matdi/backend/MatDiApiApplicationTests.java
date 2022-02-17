885e7ab5c28741651b00c38310fc9aa45f998e55
naocya@naver.com <naocya@naver.com>
2022-02-18 03:30:01 +0900

    first commit

---
+++ src/test/java/com/matdi/backend/MatDiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matdi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatDiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
