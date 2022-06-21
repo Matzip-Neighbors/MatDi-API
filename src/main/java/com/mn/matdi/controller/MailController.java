bf8994527dae2bd46d7acebdf10f249dea2ee4bd
yarogono <naocya@naver.com>
2022-06-21 13:04:29 +0900

    feat/jasypt 기능 추가

--- src/main/java/com/mn/matdi/controller/MailController.java
+++ src/main/java/com/mn/matdi/controller/MailController.java
@@ -1,28 +1,28 @@
-package com.mn.matdi.controller;
-
-import com.mn.matdi.dto.EmailRequestDto;
-import com.mn.matdi.dto.EmailVerificationNumberDto;
-import com.mn.matdi.service.EmailSenderService;
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-
-@RestController
-@RequiredArgsConstructor
-public class MailController {
-
-    private final EmailSenderService emailSenderService;
-
-    @Operation(summary = "이메일 인증번호 요청")
-    @PostMapping("/api/emailVerificationNumber")
-    public String mailSendNumber(@RequestBody EmailRequestDto.request request) {
-        return emailSenderService.sendEmail(request);
-    }
-    
-    @Operation(summary = "이메일 인증번호 검증")
-    @PostMapping("/api/emailVerificationNumber/check")
-    public Boolean mailCheckNumber(@RequestBody EmailVerificationNumberDto request) {
-        return emailSenderService.verificationEmailNumber(request);
-    }
-}
+//package com.mn.matdi.controller;
+//
+//import com.mn.matdi.dto.EmailRequestDto;
+//import com.mn.matdi.dto.EmailVerificationNumberDto;
+//import com.mn.matdi.service.EmailSenderService;
+//import io.swagger.v3.oas.annotations.Operation;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.*;
+//
+//
+//@RestController
+//@RequiredArgsConstructor
+//public class MailController {
+//
+//    private final EmailSenderService emailSenderService;
+//
+//    @Operation(summary = "이메일 인증번호 요청")
+//    @PostMapping("/api/emailVerificationNumber")
+//    public String mailSendNumber(@RequestBody EmailRequestDto.request request) {
+//        return emailSenderService.sendEmail(request);
+//    }
+//
+//    @Operation(summary = "이메일 인증번호 검증")
+//    @PostMapping("/api/emailVerificationNumber/check")
+//    public Boolean mailCheckNumber(@RequestBody EmailVerificationNumberDto request) {
+//        return emailSenderService.verificationEmailNumber(request);
+//    }
+//}
--- src/test/java/com/mn/matdi/utils/JasyptDecryptionTest.java
+++ src/test/java/com/mn/matdi/utils/JasyptDecryptionTest.java
@@ -1,2 +1,33 @@
-package com.mn.matdi.utils;public class JasyptDecryptionTest {
+package com.mn.matdi.utils;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JasyptDecryptionTest {
+
+    @Value("${jasypt.encryptor.password}")
+    private String jasyptKey;
+
+    @Autowired
+    @Qualifier("jasyptStringEncryptor")
+    private StringEncryptor jasypt;
+
+    @Test
+    void encrypt() {
+        String result = jasypt.encrypt("test");
+        System.out.println(result);
+    }
+
+
+    @Test
+    void decrypt() {
+
+        String result = jasypt.decrypt("HPgx2TR4ZB/VjO04jNiipw==");
+        System.out.println(result);
+    }
 }
