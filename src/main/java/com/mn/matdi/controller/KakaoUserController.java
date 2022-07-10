92358d8a4c7c33be2199b178554792f8d994007d
yarogono <naocya@naver.com>
2022-07-10 23:19:52 +0900

    feat/swagger summary 작성

--- src/main/java/com/mn/matdi/controller/KakaoUserController.java
+++ src/main/java/com/mn/matdi/controller/KakaoUserController.java
@@ -3,6 +3,7 @@ package com.mn.matdi.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mn.matdi.dto.KakaoUser;
 import com.mn.matdi.service.KakaoUserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ public class KakaoUserController {
 
     private final KakaoUserService kakaoUserService;
 
+    @Operation(summary = "카카오 로그인")
     @GetMapping("/api/user/kakao/callback")
     public ResponseEntity<KakaoUser.Response> kakaoLogin(@RequestParam String code) throws JsonProcessingException {
         return ResponseEntity.ok()
