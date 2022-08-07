9d8b41db3dff0ebc9fd5fb5c2d1d9b88c929d40b
yarogono <naocya@naver.com>
2022-08-07 16:40:04 +0900

    refact/UserVerificationNumberDto 정리

--- src/main/java/com/mn/matdi/dto/userVerification/UserVerificationNumberDto.java
+++ src/main/java/com/mn/matdi/dto/userVerification/UserVerificationNumberDto.java
@@ -3,9 +3,6 @@ package com.mn.matdi.dto.userVerification;
 import lombok.*;
 
 @Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserVerificationNumberDto {
     private String email;
     private String vrfNo;
